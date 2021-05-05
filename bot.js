fe4c156054ba8b3fa4aefac460a9260002636745
ritik <58474947+dwivedi-ritik@users.noreply.github.com>
2021-05-05 07:34:28 +0530

    Stupidy at its best

--- bot.js
+++ bot.js
@@ -1,20 +1,22 @@
-// import filterTweets from './twitterapi/tweetfetch'
 const filterTweets = require("./twitterapi/tweetfetch")
 const { getBotUpdate , sendResponse } = require("./Telebot/telebot")
 
-//[usrTxt , usrId , msgId]
-//sendResponse(chatId ,msgId , msg)
-
 async function wrapUp() {
+    let text
     let objs = await getBotUpdate()
     if(objs){
         filterTweets(objs[0]).then((resultObj)=>{
-            for(res of resultObj ) {
-                let text = `<strong>Tweeted By  <a href='${res["tweet_link"]}'> ${res["user_name"]}</a></strong>
-                            <pre> ${res["text"]} </pre>`
+            if(resultObj.length != 0){
+                resultObj.forEach((res)=>{
+                    text = `<strong>Tweet Link 👉 <a href='${res["tweet_link"]}'> ${res["user_name"]}</a></strong>\n\n<b> ${res["text"]}</b>`
+                    sendResponse(objs[1] , objs[2] , text)
+                })
+            }
+            else{
+                text = "<b> Ops seems like i could't find anything 🙁 \nTry searching just your city name </b>" 
                 sendResponse(objs[1] , objs[2] , text)
             }
         })
     }
 }
-setInterval(wrapUp , 5000) 
+setInterval(wrapUp , 2000) 
