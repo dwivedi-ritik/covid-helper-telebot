d8f842e489111c2479c5563e848a1710fab62721
ritik <58474947+dwivedi-ritik@users.noreply.github.com>
2021-05-09 11:32:53 +0530

    Added msg key
    for identify whether update uncludes msg or not

--- Telebot/telebot.js
+++ Telebot/telebot.js
@@ -6,7 +6,7 @@ let configData = fs.readFileSync("./config.json")
 let jsonConfig = JSON.parse(configData)
 let botToken = jsonConfig["bot_token"] 
 const TELEBOTURL = `https://api.telegram.org/bot${botToken}`
-
+const greetTxt = `<b>Hello send me your city name with requirements 🙂</b>`
 //function for sending message to user
 async function sendResponse(chatId ,msgId , msg) {
     let messageUrl = `${TELEBOTURL}/sendMessage` 
@@ -26,27 +26,33 @@ async function getBotUpdate(UPDATEID) {
         url : updateUrl,
         params : {"timeout":100 , "offset":UPDATEID}
     }
-    console.log("Getting called")
 
     let botUpdate = await axios(requestObj)
+    console.log("Getting called")
+
     let updates = botUpdate.data["result"] 
+
     if (updates.length != 0) {
         let allUpdates = []
         updates.forEach(obj=>{
-            allUpdates.push({
-                "update_id":obj["update_id"],
-                "usrTxt":obj["message"]["text"],
-                "usrId":obj["message"]["chat"]["id"],
-                "msgId":obj["message"]["message_id"]
-            })            
+            if(!("message" in obj)){
+                allUpdates.push({
+                    "update_id":obj["update_id"],
+                    "msg":false
+                })
+            }else{
+                allUpdates.push({
+                    "update_id":obj["update_id"],
+                    "usrTxt":obj["message"]["text"],
+                    "usrId":obj["message"]["chat"]["id"],
+                    "msgId":obj["message"]["message_id"],
+                    "msg":true
+                })
+            }
         })
         return allUpdates
-
     }
     return false
 }
 
 module.exports = { getBotUpdate , sendResponse }  
-
-
-
