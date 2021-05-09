c3251d4bef6e8a9f37fa5b45c643e3d401d308eb
ritik <58474947+dwivedi-ritik@users.noreply.github.com>
2021-05-09 11:31:10 +0530

    Fixed unwanted updates issues

--- bot.js
+++ bot.js
@@ -7,20 +7,23 @@ async function wrapUp() {
     if(botUpdates){
         UPDATEID = botUpdates[botUpdates.length - 1]["update_id"] + 1
         botUpdates.forEach((obj) =>{
-            filterTweets(obj["usrTxt"]).then(tweetRes =>{
-                if(!tweetRes.length){ 
-                    let opsTxt = "<b>Sorry but seems like I could't find any tweets 🙁</b>"
-                    sendResponse(obj.usrId , obj.msgId ,  opsTxt)
-                }else{
-                    tweetRes.forEach(res=>{
-                        text = `<strong>Tweet Link 👉 <a href='${res["tweet_link"]}'> ${res["user_name"]}</a></strong>\n\n<b> ${res["text"]}</b>`
-                        sendResponse(obj.usrId , obj.msgId , text)
-                    })
-                }
-            })
+            if(obj["msg"]){
+                filterTweets(obj["usrTxt"]).then(tweetRes =>{
+                    if(!tweetRes.length){ 
+                        let opsTxt = "<b>Sorry but seems like I could't find any tweets 🙁</b>"
+                        sendResponse(obj.usrId , obj.msgId ,  opsTxt)
+                    }else{
+                        tweetRes.forEach(res=>{
+                            text = `<strong>Tweet Link 👉 <a href='${res["tweet_link"]}'> ${res["user_name"]}</a></strong>\n\n<b>${res["text"]}</b>`
+                            sendResponse(obj.usrId , obj.msgId , text)
+                        })
+                    }
+                })
+            }
         })
     }
 }
+
 //This condition is used for running a asynchronous function infinelty
 //Using setInterval will crash the bot because 
 //setInterval does't give shi*t about promise :(
